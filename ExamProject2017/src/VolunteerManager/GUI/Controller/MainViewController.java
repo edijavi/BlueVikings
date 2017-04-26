2514ba51465fd66a5857d4dbde89a9a472df3cb4
kopicon <boldi123@gmail.com>
2017-04-26 12:59:01 +0200

    Basic FXMls

---
+++ ExamProject2017/src/VolunteerManager/GUI/Controller/MainViewController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VolunteerManager.GUI.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author boldi
+ */
+public class MainViewController implements Initializable {
+
+    @FXML
+    private Button btnTasks;
+    @FXML
+    private Button btnVolunteers;
+    @FXML
+    private Button btnGuilds;
+    @FXML
+    private Button btnClose;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
