e592f9114918157cc8c5bd97baeeccda59823b23
kopicon <boldi123@gmail.com>
2017-05-22 14:25:52 +0200

    Administrator view Manager editing view

---
+++ ExamProject2017/src/GUI/Controller/AdminMainViewController.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI.Controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author boldi
+ */
+public class AdminMainViewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    private Date date = new Date();
+    
+    private String[] weekdays = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+    @FXML
+    private Label lblDate;
+    @FXML
+    private Button btnVolunteers;
+    @FXML
+    private Button btnGuilds;
+    @FXML
+    private Button btnClose;
+    @FXML
+    private Button btnLogOut;
+    @FXML
+    private Button btnStatistics;
+    @FXML
+    private Button btnVolunteerStat;
+    @FXML
+    private AnchorPane paneItem;
+
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+             openItem("/GUI/View/Volunteers.fxml");
+            } catch(IOException e) {
+                System.out.println("FXML probably not found");
+                System.out.println(e);
+            }
+        setDate();
+        // TODO
+    }   
+    @FXML
+    private void pressedButton(ActionEvent event) {
+        Button pressedButton = (Button) event.getSource();
+        System.out.println("pressed button: "+event.getSource());
+        switch(pressedButton.getId()) {
+            case "btnVolunteers":
+                try {
+                    openItem("/GUI/View/Volunteers.fxml");
+                } catch(IOException e) {
+                    System.out.println("FXML probably not found");
+                    System.out.println(e);
+                }
+                
+                break;
+            case "btnStatistics":
+                try {
+                    openItem("/GUI/View/Statistics.fxml");
+                } catch(IOException e) {
+                    System.out.println("FXML probably not found");
+                    System.out.println(e);
+                }
+                break;
+            case "btnVolunteerStat":
+                try {
+                    openItem("/GUI/View/VolunteerStatistics.fxml");
+                } catch(IOException e) {
+                    System.out.println("FXML probably not found");
+                    System.out.println(e);
+                }
+                break;
+            case "btnManagers":
+                try {
+                    openItem("/GUI/View/Managers.fxml");
+                } catch(IOException e) {
+                    System.out.println("FXML probably not found");
+                    System.out.println(e);
+                }
+                break;
+            case "btnLogOut":
+                Stage stage = null;
+                stage = (Stage) btnLogOut.getScene().getWindow();
+                try{
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("GUI/View/LogIn.fxml"));
+                    Parent root = fxmlLoader.load();
+                    Scene scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+                    } catch (IOException ex)
+                    {
+                        Logger.getLogger(VolunteersController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                break;
+                
+            case "btnGuilds":
+                try
+                {
+                    openItem("/GUI/View/Guilds.fxml");
+                } catch(IOException e)
+                {
+                    System.out.println("FXML probably not found");
+                    System.out.println(e);
+                }
+                break;
+            case "btnClose":
+                System.exit(0);
+                break;
+        }
+    }
+        public void openItem(String url) throws IOException {
+        FXMLLoader itemLoader = new FXMLLoader(getClass().getResource(url));
+        AnchorPane itemPane = itemLoader.load();
+        Object controller = itemLoader.getController();
+        paneItem.getChildren().clear();
+        paneItem.getChildren().add(itemPane);
+    }
+public void setDate()
+{
+    int y = date.getYear()+1900;
+    int m = date.getMonth()+1;
+    lblDate.setText(weekdays[date.getDay()]+", "+date.getDate()+"-"+m+"-"+y);
+}
+@FXML
+public void logOutEvent(ActionEvent event)
+{
+    Stage stage = null;
+        stage = (Stage) btnLogOut.getScene().getWindow();
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("GUI/View/LogIn.fxml"));
+
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex)
+        {
+            Logger.getLogger(VolunteersController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+}
+    
+}
--- ExamProject2017/src/GUI/Controller/NewManagerController.java
+++ ExamProject2017/src/GUI/Controller/NewManagerController.java
@@ -7,7 +7,9 @@ package GUI.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +17,21 @@ import javafx.fxml.Initializable;
  * @author boldi
  */
 public class NewManagerController implements Initializable {
+
+    @FXML
+    private TextField txtFirstName;
+    @FXML
+    private TextField txtLastName;
+    @FXML
+    private TextField txtAddress;
+    @FXML
+    private TextField txtPhone;
+    @FXML
+    private TextField txtEamil;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private TextField txtUserName;
 
     /**
      * Initializes the controller class.
